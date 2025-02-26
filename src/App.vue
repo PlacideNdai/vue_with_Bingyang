a2b726492765b0205da60ba825e821c407754d04
Placide <75864037+PlacideNdai@users.noreply.github.com>
2025-02-26 11:47:08 -0600

    first lesson

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,52 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div>
+    <h1>{{ message }}</h1>
+    <hr>
+    <h1> Number: {{ number }}</h1>
+  </div>
+  <hr>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <h1>Double Number 50: {{ doubleNum(120) }}</h1>
+  <hr>
+  <h1>Number * 2 = {{ number * 2 }}</h1>
+  <hr>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>{{ number > 500 ? "Number greater than 500" : "Number is less than 500" }}</h1>
+
+  <hr>
+  <h2>{{ person }}</h2>
+
+  <hr>
+  <p v-text="number"></p>
+  <hr>
+  <h1 v-html="personGreet"></h1>
+
+
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script setup>
+// script
+
+let message = "Hello, Friends!"
+let number = 510 / 2
+
+function doubleNum(num) {
+  return num * 2;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+const person = {
+  name: "John Doe",
+  age: 30,
+  contactDetails: {
+    email: "john.doe@example.com",
+    phone: "123-456-7890"
   }
+};
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+let personGreet = "<h1>Sports<h1/>"
+</script>
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+
+
+<style scoped></style>
