cf774e2bb66295db03f139bf34a630427f062e39
Placide <75864037+PlacideNdai@users.noreply.github.com>
2025-02-26 22:05:47 -0600

    another one bruh

--- src/App.vue
+++ src/App.vue
@@ -1,88 +1,24 @@
 <template>
   <div>
     <h1>{{ message }}</h1>
-    <h1>Welcome, {{ formData.name }}</h1>
-    <form action="">
-      <table>
-        <tbody>
-          <tr>
-            <td>Name</td>
-            <td><input type="text" v-model="formData.name"></td>
-          </tr>
-          <tr>
-            <td>Gender</td>
-            <td>
-              <input type="radio" name="gender" value="female" v-model="formData.gender"> Female
-              <input type="radio" name="gender" value="male" v-model="formData.gender"> Male
-            </td>
-          </tr>
+    <p>Why did the chicken close the road?</p>
+    <button style="width: 200px;" @click="showanswer = !showanswer">{{ showanswer ? "Hide the answer" : "Show the answer"
+      }}</button>
+  </div>
 
-          <tr>
-            <td>Age</td>
-            <td><input type="number" v-model.number="formData.age"></td>
-          </tr>
-          <tr>
-            <td>Hobbies</td>
-            <td>
-              <input type="checkbox" name="hobbies" value="basketball" v-model="formData.hobbies"> Basketball
-              <input type="checkbox" name="hobbies" value="football" v-model="formData.hobbies"> Football
-              <input type="checkbox" name="hobbies" value="javascript" v-model="formData.hobbies"> JavaScript
-            </td>
-          </tr>
-          <tr>
-            <td>Profession</td>
-            <td>
-              <select name="profession" id="" v-model="formData.profession">
-                <option value="">Select one</option>
-                <option value="player">gamer</option>
-                <option value="school">profesor</option>
-              </select>
-            </td>
-          </tr>
-          <tr>
-            <td>Description</td>
-            <td>
-              <textarea name="description" id="" @keydown.enter="submitForm" v-model="formData.desc"></textarea>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      <button type="submit" @click="submitForm" >Submit</button>
-      <button type="reset" @click="resetForm">Reset</button>
-    </form>
+  <div v-if="showanswer">There was chick fia on this side.</div>
+  <div v-show="showanswer">You see me</div>
+
+  <hr>
+  <div v-if="Math.random() > 0.5">You see me</div>
+  <div v-else>Now you don't
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref } from "vue";
+let showanswer = ref(false);
 
-let formData = ref({
-  name: '',
-  gender: '',
-  age: '',
-  hobbies: [],
-  profession: '',
-  desc: ''
-})
-
-
-function submitForm() {
-  console.log(JSON.stringify(formData.value));
-}
-
-// submitting
-function resetForm() {
-  formData.value = {
-    name: '',
-    gender: '',
-    age: '',
-    hobbies: [],
-    profession: '',
-    desc: ''
-  }
-}
-
-const message = ref('Hello, Vue!');
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped></style>
