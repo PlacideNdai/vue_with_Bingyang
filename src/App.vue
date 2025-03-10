089ff4b55262035c65379ad9a96a6c2cf364951e
Placide <75864037+PlacideNdai@users.noreply.github.com>
2025-03-09 22:53:32 -0500

    vue slots named

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
 <template>
   <div>
-    <AlertBox>Invalid Creditial</AlertBox>
-    <AlertBox></AlertBox>
-    <AlertBox>
-      <h1>Request Time out</h1>
-    </AlertBox>
+    <BaseLayout>
+      <template v-slot:header>
+        <h1>Header</h1>
+      </template>
+
+
+      <template v-slot:main>
+        <h1>Main</h1>
+        <p>Something else too</p>
+      </template>
+
+
+      <template v-slot:footer>
+          <h1>Footer</h1>
+      </template>
+    </BaseLayout> 
   </div>
 </template>
 
 <script setup>
-import AlertBox from './AlertBox.vue';
+import BaseLayout from './BaseLayout.vue';
 </script>
 
 <style scoped>
