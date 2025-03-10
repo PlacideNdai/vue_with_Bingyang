ee1ffc7049ec7b0150a4a0f845a1830bc4a0894e
Placide <75864037+PlacideNdai@users.noreply.github.com>
2025-03-10 00:10:57 -0500

    slots scoped, ignore the misspelling

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,53 @@
 <template>
   <div>
-    <BaseLayout>
-      <template v-slot:header>
-        <h1>Header</h1>
+    <StudentList :list="list">
+      <template #default="{stu}">
+        <span :class="{cursed: stu.name == 'John'}">
+          {{ stu.name  }}
+        </span>
       </template>
+    </StudentList>
 
 
-      <template v-slot:main>
-        <h1>Main</h1>
-        <p>Something else too</p>
-      </template>
 
+    <hr>
 
-      <template v-slot:footer>
-          <h1>Footer</h1>
-      </template>
-    </BaseLayout> 
+    <el-table :data="todoList" stripe border style="width: 100%">
+     <el-table-column prop="userId" label="User ID" width="180" />
+      <el-table-column prop="id" label="ID" width="180" />
+      <el-table-column prop="title" label="Title" />
+      <el-table-column prop="completed" label="Status">
+        <template v-slot:default="slotProps">
+          <el-tag type="success" v-if="slotProps.row.completed">Completed</el-tag>
+          <el-tag type="danger" v-else>Incomplete</el-tag>
+        </template>
+      </el-table-column>
+  </el-table>
   </div>
 </template>
 
 <script setup>
-import BaseLayout from './BaseLayout.vue';
+import { onMounted, ref } from 'vue';
+import StudentList from './StudentList.vue';
+
+
+let todoList  = ref();
+
+async function getTodoList(){
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const data = await response.json();
+  console.log(data)
+  todoList.value = data;
+}
+
+onMounted(()=>{
+  getTodoList();
+})
+
 </script>
 
-<style scoped>
-
+<style  scoped>
+  .cursed{
+    color:red;
+  }
 </style>
